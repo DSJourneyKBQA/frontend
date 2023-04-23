7bb9e2f7f74f2e146da1272b7cf246b743a3a41f
OriLight <amatlsmisr@gmail.com>
2023-04-23 16:52:44 +0800

    add vite build info plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { vitePluginBuildInfo } from 'vite-plugin-build-info'
 
 export default defineConfig({
   resolve: {
@@ -21,6 +22,9 @@ export default defineConfig({
     Components({
       dts: true,
       directoryAsNamespace: true,
+    }),
+    vitePluginBuildInfo({
+      shortSha: true,
     }),
   ],
 })
